0c7940563ce3ab85cc57517a0580abe8c14adf32
Renato Melo <45719283+renatoam@users.noreply.github.com>
2022-03-30 09:04:48 -0300

    chore: create main project setup (#7)
    * chore(wip): install and configure storybook
    
    * feat: create folder structure
    
    * chore(wip): create new folder structure
    
    * fix: add jest config to map the new path aliases
    
    * chore: install and configure styling and design system tools

--- src/components/text/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Text() {
-  return <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor illum expedita consequatur, quod aut dolorem doloribus asperiores quidem. Error unde cum esse corrupti dolore quidem facilis tempora. Possimus, voluptate voluptatibus!</p>
-}
---
+++ src/modules/about/components/text/index.tsx
@@ -0,0 +1,10 @@
+export default function Text() {
+  return (
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor illum
+      expedita consequatur, quod aut dolorem doloribus asperiores quidem. Error
+      unde cum esse corrupti dolore quidem facilis tempora. Possimus, voluptate
+      voluptatibus!
+    </p>
+  );
+}
