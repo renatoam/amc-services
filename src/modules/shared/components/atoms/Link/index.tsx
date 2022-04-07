70dec84cf9370ce7bf2dcb1d8795ce8efdd1043d
˜Renato˜ <renato.melo@ab-inbev.com>
2022-04-07 20:32:34 -0300

    feat: create new reusable Link component

---
+++ src/modules/shared/components/atoms/Link/index.tsx
@@ -0,0 +1,18 @@
+import NextLink from "next/link";
+import { ReactNode } from "react";
+
+interface LinkProps {
+  href: string;
+  title: string;
+  children: ReactNode;
+}
+
+export default function Link(props: LinkProps) {
+  const { href, title, children } = props;
+
+  return (
+    <NextLink href={href}>
+      <a title={title}>{children}</a>
+    </NextLink>
+  );
+}
