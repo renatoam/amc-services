16253a1fc4a20c5be5472253271f4bbeb2753cfc
˜Renato˜ <renato.melo@ab-inbev.com>
2022-05-29 20:19:49 -0300

    feat: create responsive latest services section

--- src/modules/home/constants/index.ts
+++ src/modules/home/constants/index.ts
@@ -20,3 +20,24 @@ export const testimonials = [
     content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae quia quaerat modi voluptas possimus, veniam distinctio a, inventore vero vel maiores ea neque, aliquid quam quae architecto dignissimos.'
   },
 ]
+
+export const latestServices = [
+  {
+    id: uuid(),
+    title: 'Conserto de painel elétrico',
+    link: '#',
+    image: 'https://images.unsplash.com/photo-1621905251918-48416bd8575a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80',
+  },
+  {
+    id: uuid(),
+    title: 'Conserto de caixa de fuzelagem',
+    link: '#',
+    image: 'https://images.unsplash.com/photo-1635335874521-7987db781153?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    id: uuid(),
+    title: 'Reparo de caixa de luz',
+    link: '#',
+    image: 'https://images.unsplash.com/photo-1544724569-5f546fd6f2b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Contact from "@contact/components/Contact";
+import LatestServices from "@home/components/LatestServices";
 import Testimonials from "@home/components/Testimonials";
 import FAQ from "@shared/components/organisms/FAQ";
 
 export default function HomePage() {
   return (
     <>
+      <LatestServices />
       <Testimonials />
       <FAQ />
       <Contact />
